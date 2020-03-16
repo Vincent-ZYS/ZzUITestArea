234b92750d3811a95cd55ab7e337518be19e3b62
Vincent-ZYS <1654209516@qq.com>
2020-03-15 21:18:57 -0700

    Submit the basic UI scene, Resource and Script - Ready to create the "Circle Image" component

---
+++ Assets/Script/CircleImage_Component/CircleImage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleImage : Image
+{
+    private int segments;//how many segments do the cirle have
+
+    protected override void OnPopulateMesh(VertexHelper vh)
+    {
+        //vh.Clear();
+
+    }
+}

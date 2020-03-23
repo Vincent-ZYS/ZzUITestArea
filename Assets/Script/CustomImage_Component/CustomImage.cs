8e34ac8368e2e8fb47b820d05b603cc25e2ccf33
Vincent-ZYS <1654209516@qq.com>
2020-03-23 16:15:44 -0700

    Submit the precise click response of image - customized image component;

---
+++ Assets/Script/CustomImage_Component/CustomImage.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomImage : Image
+{
+    private PolygonCollider2D _polygon2D;
+
+    private PolygonCollider2D polygon2D
+    {
+        get
+        {
+            if(_polygon2D == null)
+            {
+                _polygon2D = GetComponent<PolygonCollider2D>();
+            }
+            return _polygon2D;
+        }
+    }
+
+    public override bool IsRaycastLocationValid(Vector2 screenPoint, Camera eventCamera)
+    {
+        Vector3 point;
+        RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, screenPoint, eventCamera, out point);
+        return polygon2D.OverlapPoint(point);
+    }
+}
---
+++ Assets/Script/CustomizedEditor/CustomImageEditor.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+public class CustomImageEditor : Editor
+{
+    private const int UI_LAYER = 5;
+
+    [MenuItem("GameObject/UI/CustomImage", priority = 0)]
+
+    private static void AddImage()
+    {
+        Transform canvasTf = GetCanvasTrans();
+
+        Transform image = AddCustomImage();
+
+        if(Selection.activeGameObject != null && Selection.activeGameObject.layer == UI_LAYER)
+        {
+            image.SetParent(Selection.activeGameObject.transform);
+        }else
+        {
+            image.SetParent(canvasTf);
+        }
+        image.localPosition = Vector3.zero;
+    }
+
+    private static Transform GetCanvasTrans()
+    {
+        Canvas canvas = FindObjectOfType<Canvas>();
+        if (canvas == null)
+        {
+            GameObject canvasObj = new GameObject("Canvas");
+            SetLayer(canvasObj);
+            canvasObj.AddComponent<RectTransform>();
+            canvasObj.AddComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
+            canvasObj.AddComponent<CanvasScaler>();
+            canvasObj.AddComponent<GraphicRaycaster>();
+            return canvasObj.transform;
+        }else
+        {
+            return canvas.transform;
+        }
+    }
+
+    private static Transform AddCustomImage()
+    {
+        GameObject image = new GameObject("CustomImage");
+        SetLayer(image);
+        image.AddComponent<RectTransform>();
+        image.AddComponent<PolygonCollider2D>();
+        image.AddComponent<CustomImage>();
+        return image.transform;
+    }
+
+    private static void SetLayer(GameObject uiGo)
+    {
+        uiGo.layer = UI_LAYER;
+    }
+}
